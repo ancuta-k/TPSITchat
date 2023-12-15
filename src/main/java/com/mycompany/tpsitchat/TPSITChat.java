08a45d2515f3420149f268c5b67e529e632cee80
ancuta-k <151909641+ancuta-k@users.noreply.github.com>
2023-12-15 20:48:20 +0100

    Added project

---
+++ src/main/java/com/mycompany/tpsitchat/TPSITChat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpsitchat;
+
+/**
+ *
+ * @author Karina
+ */
+public class TPSITChat {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
